6ae27648e806f0d4d2d3fa6eaccce65c35fe8d08
Aliaksei  Hrom <grom.4lek@yandex.by>
2020-06-06 00:23:46 +0300

    Refactor code from 15 lines to 7

--- spec/homework_spec.rb
+++ spec/homework_spec.rb
@@ -1,23 +1,25 @@
+# frozen_string_literal: true
+
 require './homework'
 
 describe '#validate' do
   context 'license key length' do
     context 'when > 16 digits' do
       it 'returns false' do
-        expect(validate(53082075408853231)).to eq false
+        expect(validate(53_082_075_408_853_231)).to eq false
       end
     end
 
     context 'when < 16 digits' do
       it 'returns false' do
-        expect(validate(53082075408853)).to eq false
+        expect(validate(53_082_075_408_853)).to eq false
       end
     end
   end
 
   context 'first two digits of license key' do
-    [4169294814153321, 0244403223651176, 1364967124577676, 5036402220169339,
-     8539470583285584].each do |key|
+    [4_169_294_814_153_321, 0o244403223651176, 1_364_967_124_577_676, 5_036_402_220_169_339,
+     8_539_470_583_285_584].each do |key|
       it 'returns false' do
         expect(validate(key)).to eq false
       end
@@ -25,8 +27,8 @@ describe '#validate' do
   end
 
   context 'checksum' do
-    [5169294814153321, 5244403223651176, 5364967124577676,
-     5436402220169339, 5539470583285584].each do |key|
+    [5_169_294_814_153_321, 5_244_403_223_651_176, 5_364_967_124_577_676,
+     5_436_402_220_169_339, 5_539_470_583_285_584].each do |key|
       it 'returns true' do
         expect(validate(key)).to eq true
       end
